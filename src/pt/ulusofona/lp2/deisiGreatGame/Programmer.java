29a9f3537edd3c809b498c121241d300c6cc5a15
rafaelsimoesa22005793 <a22005793@alunos.ulht.pt>
2021-11-10 11:10:09 +0000

    Acabei o toString da classe Programmer e acrescentamos mais alguns métodos.

--- src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
@@ -1,14 +1,24 @@
 package pt.ulusofona.lp2.deisiGreatGame;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Programmer {
     int id;
     String name;
     ProgrammerColor color;
-    int pos;
+    int pos = 0;
     ArrayList<String> linguagensProg = new ArrayList<>();
     boolean estado = true;
+    boolean turno = false;
+
+
+    public Programmer(int id, String name, ProgrammerColor color, ArrayList<String> linguagensProg) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.linguagensProg = linguagensProg;
+    }
 
     public int getId() {
         return id;
@@ -34,8 +44,30 @@ public class Programmer {
         }
     }
 
+    public void derrotado(){
+        if (estado == false){
+            return;
+        } else {
+            estado = false;
+        }
+    }
+
     public String getLinguagensProg(){
-        return "";
+        Collections.sort(linguagensProg);
+        StringBuilder linguagens = new StringBuilder();
+        for (String linguagem : linguagensProg){
+            linguagens.append(linguagem);
+            linguagens.append("; ");
+        }
+        return linguagens.substring(0,linguagens.length() - 2);
+    }
+
+    public void trocarTurno(){
+        if (turno){
+            turno = false;
+        } else {
+            turno = true;
+        }
     }
 
     @Override
